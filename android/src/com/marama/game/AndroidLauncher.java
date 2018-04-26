d62c6a24a560bc644f28217a5b7f92fcf3cba9fa
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2018-04-26 12:00:55 +0200

    Added libGDX

---
+++ android/src/com/marama/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.marama.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.marama.game.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
