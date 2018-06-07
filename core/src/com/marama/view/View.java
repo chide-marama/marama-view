2c8118c55464f3a80c0404c80c4ca548353f491d
jjkramok <jjkramok@gmail.com>
2018-06-07 10:30:58 +0200

    removed unused import

--- core/src/com/marama/view/View.java
+++ core/src/com/marama/view/View.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 import com.marama.view.screens.SplashScreen;
 
 /**
